ee69b964e6ad0a1717fd7919be3553988941983a
kfyty <kfyty725@hotmail.com>
2019-08-09 15:01:11 +0800

    没啥，就是想 push 一下

--- mybatis-demo/src/main/java/com/kfyty/annotation/SelectOne.java
+++ mybatis-demo/src/main/java/com/kfyty/annotation/SelectOne.java
@@ -2,7 +2,11 @@ package com.kfyty.annotation;
 
 import com.kfyty.annotation.container.SelectOnes;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
