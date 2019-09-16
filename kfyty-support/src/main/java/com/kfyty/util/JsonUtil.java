87b07b8f9a9b98cd9880999c5c8db67b6d5e570e
kfyty <kfyty725@hotmail.com>
2019-09-16 18:11:14 +0800

    设置 jackson 对日期格式的转换

--- kfyty-support/src/main/java/com/kfyty/util/JsonUtil.java
+++ kfyty-support/src/main/java/com/kfyty/util/JsonUtil.java
@@ -2,8 +2,10 @@ package com.kfyty.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * 功能描述: json 工具类
@@ -13,13 +15,26 @@ import java.io.IOException;
  * @since JDK 1.8
  */
 public class JsonUtil {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static final ObjectMapper OBJECT_READ_MAPPER = new ObjectMapper();
+
+    private static final ObjectMapper OBJECT_WRITE_MAPPER = new ObjectMapper();
+
+    static {
+        OBJECT_WRITE_MAPPER.configure(WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+        OBJECT_WRITE_MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+    }
 
     public static String convert2Json(Object o) throws JsonProcessingException {
-        return OBJECT_MAPPER.writeValueAsString(o);
+        return OBJECT_WRITE_MAPPER.writeValueAsString(o);
+    }
+
+    public static String convert2Json(Object o, String dateFormat) throws JsonProcessingException {
+        OBJECT_WRITE_MAPPER.setDateFormat(new SimpleDateFormat(dateFormat));
+        return OBJECT_WRITE_MAPPER.writeValueAsString(o);
     }
 
     public static <T> T convert2Object(String json, Class<T> clazz) throws IOException {
-        return OBJECT_MAPPER.readValue(json, clazz);
+        return OBJECT_READ_MAPPER.readValue(json, clazz);
     }
 }
