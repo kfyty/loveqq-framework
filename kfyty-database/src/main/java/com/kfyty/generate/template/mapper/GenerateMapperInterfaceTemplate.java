89f19dfe29d6a8dfa9561e8da218f995a598a1ef
kfyty <kfyty725@hotmail.com>
2019-09-24 18:34:43 +0800

    update

--- kfyty-database/src/main/java/com/kfyty/generate/template/mapper/GenerateMapperInterfaceTemplate.java
+++ kfyty-database/src/main/java/com/kfyty/generate/template/mapper/GenerateMapperInterfaceTemplate.java
@@ -3,7 +3,6 @@ package com.kfyty.generate.template.mapper;
 import com.kfyty.generate.GenerateSourcesBufferedWriter;
 import com.kfyty.generate.info.AbstractDataBaseInfo;
 import com.kfyty.generate.template.pojo.GeneratePojoTemplate;
-import com.kfyty.util.CommonUtil;
 
 import java.io.IOException;
 
@@ -50,22 +49,17 @@ public class GenerateMapperInterfaceTemplate extends GeneratePojoTemplate {
     }
 
     public void generateMapperInterfaces(AbstractDataBaseInfo dataBaseInfo, String basePackage, GenerateSourcesBufferedWriter out) throws IOException {
-        String varName = CommonUtil.convert2Hump(dataBaseInfo.getTableName(), false);
-        String className = CommonUtil.convert2Hump(dataBaseInfo.getTableName(), true);
-        String entityVarName = varName + entityFileSuffix();
-        String entityClassName = className + entityFileSuffix();
+        out.writeLine("\n\t{} findById(@Param(\"id\") String id) throws SQLException;\n", this.entityClassName);
 
-        out.writeLine("\t{} findById(@Param(\"id\") String id) throws SQLException;\n", entityClassName);
+        out.writeLine("\tList<{}> findBy(@Param(\"field\") String field, @Param(\"value\") Object value) throws SQLException;\n", this.entityClassName);
 
-        out.writeLine("\tList<{}> findBy(@Param(\"field\") String field, @Param(\"value\") Object value) throws SQLException;\n", entityClassName);
+        out.writeLine("\tList<{}> findLike(@Param(\"field\") String field, @Param(\"value\") Object value) throws SQLException;\n", this.entityClassName);
 
-        out.writeLine("\tList<{}> findLike(@Param(\"field\") String field, @Param(\"value\") Object value) throws SQLException;\n", entityClassName);
+        out.writeLine("\tList<{}> findAll() throws SQLException;\n", this.entityClassName);
 
-        out.writeLine("\tList<{}> findAll() throws SQLException;\n", entityClassName);
+        out.writeLine("\tvoid insert(@Param(\"{}\") {} {}) throws SQLException;\n", this.entityClassVariableName, this.entityClassName, this.entityClassVariableName);
 
-        out.writeLine("\tvoid insert(@Param(\"{}\") {} {}) throws SQLException;\n", entityVarName, entityClassName, entityVarName);
-
-        out.writeLine("\tvoid updateById(@Param(\"id\") String id, @Param(\"{}\") {} {}) throws SQLException;\n", entityVarName, entityClassName, entityVarName);
+        out.writeLine("\tvoid updateById(@Param(\"id\") String id, @Param(\"{}\") {} {}) throws SQLException;\n", this.entityClassVariableName, this.entityClassName, this.entityClassVariableName);
 
         out.writeLine("\tvoid deleteById(@Param(\"id\") String id) throws SQLException;\n");
     }
