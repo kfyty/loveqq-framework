bfd63e938ca13755f72315bb598a3ef0501bfa9c
kfyty <kfyty725@hotmail.com>
2021-09-17 20:46:27 +0800

    feat:lambda 序列化工具

---
+++ kfyty-support/src/main/java/com/kfyty/support/wrapper/SerializableFunction.java
@@ -0,0 +1,15 @@
+package com.kfyty.support.wrapper;
+
+import java.io.Serializable;
+
+/**
+ * 描述: 可序列化的函数式接口
+ *
+ * @author kfyty725
+ * @date 2021/9/17 20:37
+ * @email kfyty725@hotmail.com
+ */
+@FunctionalInterface
+public interface SerializableFunction<T, R> extends Serializable {
+    R apply(T t);
+}
