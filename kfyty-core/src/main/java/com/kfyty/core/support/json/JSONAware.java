0ecd66da2079d8d059f8ee157d2c39dbc199269a
kfyty725 <kfyty725@hotmail.com>
2023-07-13 00:07:09 +0800

    feat:json

---
+++ kfyty-core/src/main/java/com/kfyty/core/support/json/JSONAware.java
@@ -0,0 +1,36 @@
+package com.kfyty.core.support.json;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 描述: JSON Aware
+ *
+ * @author kfyty725
+ * @date 2022/7/17 18:46
+ * @email kfyty725@hotmail.com
+ */
+public interface JSONAware {
+
+    @SuppressWarnings("unchecked")
+    default JSON getJSON(Object o, String msg) {
+        if (o == null || o instanceof JSON) {
+            return (JSON) o;
+        }
+        if (o instanceof Map<?, ?>) {
+            return new JSON((Map<String, Object>) o);
+        }
+        throw new IllegalStateException(msg);
+    }
+
+    @SuppressWarnings("unchecked")
+    default Array getArray(Object o, String msg) {
+        if (o == null || o instanceof Array) {
+            return (Array) o;
+        }
+        if (o instanceof List<?>) {
+            return new Array((List<Object>) o);
+        }
+        throw new IllegalStateException(msg);
+    }
+}
