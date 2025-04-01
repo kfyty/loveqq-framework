3ac8218ca896b5bc1c51838cfe5f78877c9e102b
kfyty725 <kfyty725@hotmail.com>
2025-04-01 23:23:23 +0800

    opt:自定义配置上下文

--- loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/ConfigurableApplicationContext.java
+++ loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/ConfigurableApplicationContext.java
@@ -16,33 +16,118 @@ import java.util.Set;
  * @email kfyty725@hotmail.com
  */
 public interface ConfigurableApplicationContext extends ApplicationContext {
-
+    /**
+     * 获取命令行参数
+     *
+     * @return 命令行参数
+     */
     String[] getCommandLineArgs();
 
+    /**
+     * 获取路径匹配器
+     *
+     * @return 路径匹配器
+     */
     PatternMatcher getPatternMatcher();
 
+    /**
+     * 获取自定义配置的资源
+     *
+     * @return 自定义配置
+     * @see Class
+     * @see ComponentFilterDescription
+     * @see ComponentMatcher
+     */
+    List<Object> getSources();
+
+    /**
+     * 获取已扫描的 class
+     *
+     * @return 已扫描的 class
+     */
     Set<Class<?>> getScannedClasses();
 
+    /**
+     * 获取包含组件过滤器
+     *
+     * @return 组件过滤器
+     */
     List<ComponentFilterDescription> getIncludeFilters();
 
+    /**
+     * 获取排除组件过滤器
+     *
+     * @return 组件过滤器
+     */
     List<ComponentFilterDescription> getExcludeFilters();
 
+    /**
+     * 获取组件匹配器
+     *
+     * @return 组件匹配器
+     */
     List<ComponentMatcher> getComponentMatcher();
 
+    /**
+     * 设置命令行参数
+     *
+     * @param args 命令行参数
+     */
     void setCommandLineArgs(String[] args);
 
+    /**
+     * 设置启动类资源
+     *
+     * @param primarySource 启动类资源
+     */
     void setPrimarySource(Class<?> primarySource);
 
+    /**
+     * 设置路径匹配器
+     *
+     * @param patternMatcher 路径匹配器
+     */
     void setPatternMatcher(PatternMatcher patternMatcher);
 
+    /**
+     * 添加自定义配置的资源
+     *
+     * @param source 自定义配置的资源
+     * @see Class
+     * @see ComponentFilterDescription
+     * @see ComponentMatcher
+     */
+    void addSource(Object source);
+
+    /**
+     * 添加自定义配置的资源
+     *
+     * @param sources 自定义配置的资源
+     * @see Class
+     * @see ComponentFilterDescription
+     * @see ComponentMatcher
+     */
+    void addSources(Collection<Object> sources);
+
+    /**
+     * 添加已扫描的 class
+     *
+     * @param clazz 已扫描的 class
+     */
     void addScannedClass(Class<?> clazz);
 
-    void addScannedClasses(Collection<Class<?>> classes);
+    /**
+     * 添加组件过滤器
+     *
+     * @param componentFilter 组件过滤器
+     */
+    void addComponentFilter(ComponentFilterDescription componentFilter);
 
-    void addIncludeFilter(ComponentFilterDescription componentFilter);
-
-    void addExcludeFilter(ComponentFilterDescription componentFilter);
-
+    /**
+     * 添加组件匹配器
+     *
+     * @param componentMatcher 组件匹配器
+     */
     void addComponentMatcher(ComponentMatcher componentMatcher);
 
     /**
