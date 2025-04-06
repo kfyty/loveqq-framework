42b23af0d3c88edaa16ae11fddcdafbbc17378f0
kfyty725 <kfyty725@hotmail.com>
2025-04-06 19:11:18 +0800

    opt:mvc性能

--- loveqq-mvc/loveqq-mvc-core/src/main/java/com/kfyty/loveqq/framework/web/core/interceptor/ReactiveHandlerInterceptor.java
+++ loveqq-mvc/loveqq-mvc-core/src/main/java/com/kfyty/loveqq/framework/web/core/interceptor/ReactiveHandlerInterceptor.java
@@ -15,16 +15,14 @@ import reactor.core.publisher.Mono;
 public interface ReactiveHandlerInterceptor extends HandlerInterceptor {
 
     default Mono<Boolean> preHandleAsync(ServerRequest request, ServerResponse response, MethodMapping handler) {
-        return Mono.just(this.preHandle(request, response, handler));
+        return Mono.fromSupplier(() -> this.preHandle(request, response, handler));
     }
 
     default Mono<Void> postHandleAsync(ServerRequest request, ServerResponse response, MethodMapping handler, Object retValue) {
-        this.postHandle(request, response, handler, retValue);
-        return Mono.empty();
+        return Mono.fromRunnable(() -> this.postHandle(request, response, handler, retValue));
     }
 
     default Mono<Void> afterCompletionAsync(ServerRequest request, ServerResponse response, MethodMapping handler, Throwable ex) {
-        this.afterCompletion(request, response, handler, ex);
-        return Mono.empty();
+        return Mono.fromRunnable(() -> this.afterCompletion(request, response, handler, ex));
     }
 }
