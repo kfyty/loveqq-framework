b4dd3de9d81fd8f8c590b2fe638dbcd74caf3369
kfyty725 <kfyty725@hotmail.com>
2025-04-26 20:11:36 +0800

    opt:mail自动配置

--- loveqq-boot-starter-mail/src/main/java/com/kfyty/loveqq/framework/boot/mail/autoconfig/MailService.java
+++ loveqq-boot-starter-mail/src/main/java/com/kfyty/loveqq/framework/boot/mail/autoconfig/MailService.java
@@ -1,7 +1,9 @@
 package com.kfyty.loveqq.framework.boot.mail.autoconfig;
 
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.Autowired;
+import com.kfyty.loveqq.framework.core.utils.CommonUtil;
 import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
 import jakarta.activation.FileDataSource;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
@@ -14,10 +16,12 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
+import jakarta.mail.util.ByteArrayDataSource;
 import lombok.SneakyThrows;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 描述: 邮件服务
@@ -71,11 +75,22 @@ public class MailService {
      * @param receiveUser 接受者
      * @param multipart   内容
      */
-    @SneakyThrows({AddressException.class, MessagingException.class})
     public void send(String subject, String receiveUser, Multipart multipart) {
+        this.send(subject, receiveUser, multipart, Message.RecipientType.TO);
+    }
+
+    /**
+     * 发送邮件
+     *
+     * @param subject     主题
+     * @param receiveUser 接受者
+     * @param multipart   内容
+     */
+    @SneakyThrows({AddressException.class, MessagingException.class})
+    public void send(String subject, String receiveUser, Multipart multipart, Message.RecipientType recipientType) {
         MimeMessage mimeMessage = new MimeMessage(this.mailSession);
         mimeMessage.setFrom(new InternetAddress(this.mailProperties.getUsername()));
-        mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(receiveUser));
+        mimeMessage.setRecipient(recipientType, new InternetAddress(receiveUser));
         mimeMessage.setSubject(subject);
         mimeMessage.setContent(multipart);
         mimeMessage.saveChanges();
@@ -89,8 +104,12 @@ public class MailService {
      */
     @SneakyThrows({NoSuchProviderException.class, MessagingException.class})
     public void send(MimeMessage mimeMessage) {
-        try (Transport transport = this.mailSession.getTransport()) {
-            transport.connect(this.mailProperties.getHost(), this.mailProperties.getUsername(), this.mailProperties.getPassword());
+        try (Transport transport = CommonUtil.empty(this.mailProperties.getProtocol()) ? this.mailSession.getTransport() : this.mailSession.getTransport(this.mailProperties.getProtocol())) {
+            if (this.mailProperties.getPort() == null || this.mailProperties.getPort() < 0) {
+                transport.connect(this.mailProperties.getHost(), this.mailProperties.getUsername(), this.mailProperties.getPassword());
+            } else {
+                transport.connect(this.mailProperties.getHost(), this.mailProperties.getPort(), this.mailProperties.getUsername(), this.mailProperties.getPassword());
+            }
             transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
         }
     }
@@ -104,19 +123,55 @@ public class MailService {
      * @param files       附件
      */
     public void sendFiles(String subject, String receiveUser, String content, List<File> files) {
-        MimeMultipart multipart = packageMultiPart(content, files);
+        List<DataSource> dataSources = files.stream().map(FileDataSource::new).collect(Collectors.toList());
+        MimeMultipart multipart = packageMultiPart(content, dataSources);
         this.send(subject, receiveUser, multipart);
     }
 
+    /**
+     * 发送附件
+     *
+     * @param subject     主题
+     * @param receiveUser 接收人
+     * @param content     内容
+     * @param contentType 附件内容
+     * @param bytes       附件
+     */
+    public void sendBytes(String subject, String receiveUser, String content, String contentType, List<byte[]> bytes) {
+        List<DataSource> dataSources = bytes.stream().map(e -> new ByteArrayDataSource(e, contentType)).collect(Collectors.toList());
+        MimeMultipart multipart = packageMultiPart(content, dataSources);
+        this.send(subject, receiveUser, multipart);
+    }
+
+    /**
+     * 发送附件
+     *
+     * @param subject     主题
+     * @param receiveUser 接收人
+     * @param content     内容
+     * @param dataSources 附件
+     */
+    public void sendDataSources(String subject, String receiveUser, String content, List<DataSource> dataSources) {
+        MimeMultipart multipart = packageMultiPart(content, dataSources);
+        this.send(subject, receiveUser, multipart);
+    }
+
+    /**
+     * 打包附件
+     *
+     * @param content     邮件内容
+     * @param dataSources 邮件附件数据源
+     * @return {@link MimeMultipart}
+     */
     @SneakyThrows(MessagingException.class)
-    public static MimeMultipart packageMultiPart(String content, List<File> files) {
+    public static MimeMultipart packageMultiPart(String content, List<DataSource> dataSources) {
         MimeMultipart multipart = new MimeMultipart();
         multipart.setSubType("mixed");
 
-        for (File file : files) {
+        for (DataSource dataSource : dataSources) {
             MimeBodyPart bodyPart = new MimeBodyPart();
-            bodyPart.setFileName(file.getName());
-            bodyPart.setDataHandler(new DataHandler(new FileDataSource(file)));
+            bodyPart.setFileName(dataSource.getName());
+            bodyPart.setDataHandler(new DataHandler(dataSource));
             multipart.addBodyPart(bodyPart);
         }
 
