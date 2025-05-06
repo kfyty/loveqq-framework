8e501ab49f2923b946b2b87e885fbd3e62eda89c
kfyty725 <kfyty725@hotmail.com>
2025-05-06 21:12:36 +0800

    feat:ResponseBodyEmitter

---
+++ loveqq-mvc/loveqq-mvc-core/src/main/java/com/kfyty/loveqq/framework/web/core/request/support/ResponseBodyEmitter.java
@@ -0,0 +1,285 @@
+package com.kfyty.loveqq.framework.web.core.request.support;
+
+import com.kfyty.loveqq.framework.core.exception.ResolvableException;
+import com.kfyty.loveqq.framework.core.utils.JsonUtil;
+import com.kfyty.loveqq.framework.core.utils.NIOUtil;
+import com.kfyty.loveqq.framework.web.core.exception.AsyncTimeoutException;
+import io.netty.buffer.ByteBuf;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Flow;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * 描述: response body emitter，目前 reactor-netty web服务器支持
+ *
+ * @author kfyty725
+ * @date 2021/6/10 11:50
+ * @email kfyty725@hotmail.com
+ */
+public class ResponseBodyEmitter {
+    /**
+     * 超时异常
+     */
+    public static final AsyncTimeoutException ASYNC_TIMEOUT_EXCEPTION = new AsyncTimeoutException("Async request timed out");
+
+    /**
+     * 默认转换器
+     * 可进行设置
+     */
+    public static Function<Object, ByteBuf> DEFAULT_CONVERTER = data -> {
+        if (data instanceof ByteBuf) {
+            return (ByteBuf) data;
+        }
+        if (data instanceof Boolean) {
+            return NIOUtil.from((Boolean) data);
+        }
+        if (data instanceof Number) {
+            return NIOUtil.from((Number) data);
+        }
+        if (data instanceof CharSequence) {
+            return NIOUtil.from(data.toString().getBytes(StandardCharsets.UTF_8));
+        }
+        if (data instanceof byte[]) {
+            return NIOUtil.from((byte[]) data);
+        }
+        return NIOUtil.from(JsonUtil.toJSONString(data).getBytes(StandardCharsets.UTF_8));
+    };
+
+    /**
+     * 是否完成
+     */
+    @Getter
+    protected boolean done;
+
+    /**
+     * 数据发布是否已完成
+     */
+    @Getter
+    protected boolean complete;
+
+    /**
+     * 是否取消
+     */
+    @Getter
+    protected boolean cancelled;
+
+    /**
+     * 超时时间，大于 0 有效
+     */
+    @Getter
+    protected long timeout;
+
+    /**
+     * 异常
+     */
+    protected Throwable failure;
+
+    /**
+     * 早期数据
+     */
+    protected List<Object> earlyData;
+
+    /**
+     * 订阅者
+     */
+    protected Flow.Subscriber<? super ByteBuf> s;
+
+    /**
+     * 数据转换器
+     */
+    protected Function<Object, ByteBuf> converter;
+
+    /**
+     * 超时回调
+     * 该超时是指从发起请求开始，到请求完成的超时时间
+     */
+    protected Runnable timeoutCallback;
+
+    /**
+     * 异常回调
+     */
+    protected Consumer<Throwable> errorCallback;
+
+    /**
+     * 完成回调
+     */
+    protected Runnable completionCallback;
+
+    public ResponseBodyEmitter() {
+        this.earlyData = new LinkedList<>();
+    }
+
+    public ResponseBodyEmitter(long timeout) {
+        this.timeout = timeout;
+        this.earlyData = new LinkedList<>();
+    }
+
+    public void setConverter(Function<Object, ByteBuf> converter) {
+        this.converter = Objects.requireNonNull(converter);
+    }
+
+    public void send(Object data) {
+        if (!this.done) {
+            if (this.s == null) {
+                this.earlyData.add(data);
+            } else if (!this.cancelled) {
+                Function<Object, ByteBuf> converter = this.converter != null ? this.converter : DEFAULT_CONVERTER;
+                this.s.onNext(converter.apply(data));
+            }
+        }
+    }
+
+    public synchronized void completeWithTimeout() {
+        if (this.done || this.cancelled) {
+            return;
+        }
+
+        // 数据发布完成
+        this.complete = true;
+        this.failure = ASYNC_TIMEOUT_EXCEPTION;
+
+        if (this.s == null) {
+            return;
+        }
+
+        this.done = true;
+
+        if (this.timeoutCallback != null) {
+            try {
+                this.timeoutCallback.run();
+            } catch (Throwable e) {
+                this.s.onError(new AsyncTimeoutException("Async request timed out, nested exception: " + e.getMessage(), e));
+                return;
+            }
+        }
+
+        this.s.onError(new AsyncTimeoutException("Async request timed out"));
+    }
+
+    public synchronized void completeWithError(Throwable error) {
+        if (this.done) {
+            return;
+        }
+
+        // 数据发布完成
+        this.complete = true;
+        this.failure = error;
+
+        if (this.s == null) {
+            return;
+        }
+
+        this.done = true;
+
+        if (this.errorCallback != null) {
+            try {
+                this.errorCallback.accept(error);
+            } catch (Throwable ex) {
+                this.s.onError(new ResolvableException(error.getMessage() + ", nested exception: " + ex.getMessage(), ex));
+                return;
+            }
+        }
+
+        this.s.onError(error);
+    }
+
+    public synchronized void complete() {
+        if (this.done) {
+            return;
+        }
+
+        // 数据发布完成
+        this.complete = true;
+
+        if (this.s == null) {
+            return;
+        }
+
+        this.done = true;
+
+        if (this.completionCallback != null) {
+            try {
+                this.completionCallback.run();
+            } catch (Throwable e) {
+                this.s.onError(e);
+                return;
+            }
+        }
+
+        if (!this.cancelled) {
+            this.s.onComplete();
+        }
+    }
+
+    public ResponseBodyEmitter onTimeout(Runnable timeoutCallback) {
+        this.timeoutCallback = timeoutCallback;
+        return this;
+    }
+
+    public ResponseBodyEmitter onError(Consumer<Throwable> callback) {
+        this.errorCallback = callback;
+        return this;
+    }
+
+    public ResponseBodyEmitter onCompletion(Runnable callback) {
+        this.completionCallback = callback;
+        return this;
+    }
+
+    public Flow.Publisher<ByteBuf> toPublisher() {
+        return new ResponseBodyEmitterPublisher();
+    }
+
+    protected synchronized void sendEarlyData() {
+        if (!this.earlyData.isEmpty()) {
+            try {
+                for (Object data : this.earlyData) {
+                    this.send(data);
+                }
+            } finally {
+                this.earlyData.clear();
+            }
+        }
+    }
+
+    @RequiredArgsConstructor
+    private class ResponseBodyEmitterPublisher implements Flow.Publisher<ByteBuf> {
+
+        @Override
+        public void subscribe(Flow.Subscriber<? super ByteBuf> s) {
+            ResponseBodyEmitter.this.s = s;
+            s.onSubscribe(new ResponseBodyEmitterSubscription());
+        }
+    }
+
+    @RequiredArgsConstructor
+    private class ResponseBodyEmitterSubscription implements Flow.Subscription {
+
+        @Override
+        public void request(long n) {
+            sendEarlyData();
+
+            if (complete) {
+                if (failure == null) {
+                    complete();
+                } else if (failure == ASYNC_TIMEOUT_EXCEPTION) {
+                    completeWithTimeout();
+                } else {
+                    completeWithError(failure);
+                }
+            }
+        }
+
+        @Override
+        public void cancel() {
+            cancelled = true;
+        }
+    }
+}
--- loveqq-mvc/loveqq-mvc-core/src/main/java/com/kfyty/loveqq/framework/web/core/request/support/SseEmitter.java
+++ loveqq-mvc/loveqq-mvc-core/src/main/java/com/kfyty/loveqq/framework/web/core/request/support/SseEmitter.java
@@ -1,13 +1,9 @@
 package com.kfyty.loveqq.framework.web.core.request.support;
 
-import com.kfyty.loveqq.framework.core.exception.ResolvableException;
 import io.netty.buffer.ByteBuf;
-import lombok.RequiredArgsConstructor;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Flow;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * 描述: sse emitter，目前 reactor-netty web服务器支持
@@ -16,137 +12,38 @@ import java.util.function.Consumer;
  * @date 2021/6/10 11:50
  * @email kfyty725@hotmail.com
  */
-public class SseEmitter {
+public class SseEmitter extends ResponseBodyEmitter {
     /**
-     * 是否取消
+     * 默认转换器
+     * 可进行设置
      */
-    private boolean cancelled;
-
-    /**
-     * 是否完成
-     */
-    private boolean done;
-
-    /**
-     * 早期数据
-     */
-    private List<SseEventStream> earlyData;
-
-    /**
-     * 订阅者
-     */
-    private Flow.Subscriber<? super ByteBuf> s;
-
-    /**
-     * 完成回调
-     */
-    private Runnable completionCallback;
-
-    /**
-     * 异常回调
-     */
-    private Consumer<Throwable> errorCallback;
+    public static Function<Object, ByteBuf> DEFAULT_CONVERTER = data -> data instanceof SseEventStream ? ((SseEventStream) data).build() : SseEventStream.builder().data(data).build().build();
 
     public SseEmitter() {
-        this.earlyData = new LinkedList<>();
+        super();
+        this.setConverter(DEFAULT_CONVERTER);
     }
 
-    public void send(Object data) {
-        this.send(SseEventStream.builder().data(data).build());
+    public SseEmitter(long timeout) {
+        super(timeout);
+        this.setConverter(DEFAULT_CONVERTER);
     }
 
-    public void send(SseEventStream sse) {
-        if (!this.done) {
-            if (this.s == null) {
-                this.earlyData.add(sse);
-            } else if (!this.cancelled) {
-                this.s.onNext(sse.build());
-            }
-        }
-    }
-
-    public synchronized void complete() {
-        if (this.done) {
-            return;
-        }
-
-        this.done = true;
-
-        try {
-            if (this.completionCallback != null) {
-                this.completionCallback.run();
-            }
-        } catch (Throwable e) {
-            this.s.onError(e);
-        }
-
-        if (!this.cancelled) {
-            this.s.onComplete();
-        }
-    }
-
-    public synchronized void completeWithError(Throwable error) {
-        if (this.done) {
-            return;
-        }
-
-        this.done = true;
-
-        try {
-            if (this.errorCallback != null) {
-                this.errorCallback.accept(error);
-            }
-        } catch (Throwable ex) {
-            this.s.onError(new ResolvableException(error.getMessage() + ", nested exception: " + ex.getMessage(), ex));
-        }
-
-        this.s.onError(error);
-    }
-
-    public SseEmitter onCompletion(Runnable callback) {
-        this.completionCallback = callback;
+    @Override
+    public SseEmitter onTimeout(Runnable timeoutCallback) {
+        super.onTimeout(timeoutCallback);
         return this;
     }
 
+    @Override
     public SseEmitter onError(Consumer<Throwable> callback) {
-        this.errorCallback = callback;
+        super.onError(callback);
         return this;
     }
 
-    public Flow.Publisher<ByteBuf> toPublisher() {
-        return new SseEmitterPublisher();
-    }
-
-    protected synchronized void sendEarlyData() {
-        if (!this.earlyData.isEmpty()) {
-            for (SseEventStream data : this.earlyData) {
-                this.s.onNext(data.build());
-            }
-            this.earlyData.clear();
-        }
-    }
-
-    @RequiredArgsConstructor
-    private class SseEmitterPublisher implements Flow.Publisher<ByteBuf> {
-
-        @Override
-        public void subscribe(Flow.Subscriber<? super ByteBuf> s) {
-            SseEmitter.this.s = s;
-            s.onSubscribe(new SseEmitterSubscription());
-        }
-    }
-
-    @RequiredArgsConstructor
-    private class SseEmitterSubscription implements Flow.Subscription {
-
-        @Override
-        public void request(long n) {
-            sendEarlyData();
-        }
-
-        @Override
-        public void cancel() {
-            cancelled = true;
-        }
+    @Override
+    public SseEmitter onCompletion(Runnable callback) {
+        super.onCompletion(callback);
+        return this;
     }
 }
