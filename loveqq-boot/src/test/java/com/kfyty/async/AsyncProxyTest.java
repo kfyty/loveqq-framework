abdb651d493371e2c796748367345d9c27d12ce6
kfyty725 <kfyty725@hotmail.com>
2025-06-15 21:11:00 +0800

    feat:Async.Await 支持异步转同步

--- loveqq-boot/src/test/java/com/kfyty/async/AsyncProxyTest.java
+++ loveqq-boot/src/test/java/com/kfyty/async/AsyncProxyTest.java
@@ -7,6 +7,9 @@ import com.kfyty.loveqq.framework.core.autoconfig.annotation.BootApplication;
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.Component;
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.EventListener;
 import com.kfyty.loveqq.framework.core.event.ContextRefreshedEvent;
+import com.kfyty.loveqq.framework.core.utils.CommonUtil;
+import lombok.Getter;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -37,17 +40,37 @@ public class AsyncProxyTest {
         asyncService.test();
 
         Assertions.assertEquals(asyncService.testFuture().get(), 1);
-
         Assertions.assertEquals(asyncService.testCompletionStage().toCompletableFuture().get(), 1);
+
+        Assertions.assertTrue(this.asyncService.awaitTest(asyncService));
+        Assertions.assertEquals(this.asyncService.awaitTest2(asyncService), 3);
     }
 
     @Async
+    @Getter
     @Component
     static class AsyncService {
+        boolean flag = false;
+
+        @Async.Await
+        public boolean awaitTest(AsyncService asyncService) {
+            asyncService.test();
+            return asyncService.isFlag();
+        }
+
+        @Async.Await
+        @SneakyThrows
+        public Integer awaitTest2(AsyncService asyncService) {
+            Future<Integer> future = asyncService.testFuture();
+            CompletionStage<Integer> stage = asyncService.testCompletionStage();
+            Future<Integer> testNoAnnotation = asyncService.testNoAnnotation();
+            return future.get() + stage.toCompletableFuture().get() + testNoAnnotation.get();
+        }
 
         @Async
         void test() {
-            System.out.println(Thread.currentThread().getName());
+            CommonUtil.sleep(500);
+            flag = true;
         }
 
         @Async
@@ -58,6 +81,11 @@ public class AsyncProxyTest {
         @Async
         CompletionStage<Integer> testCompletionStage() {
             return CompletableFuture.completedStage(1);
+        }
+
+        Future<Integer> testNoAnnotation() {
+            CommonUtil.sleep(500);
+            return CompletableFuture.completedFuture(1);
         }
     }
 }
--- loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/annotation/Async.java
+++ loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/annotation/Async.java
@@ -15,13 +15,36 @@ import java.lang.annotation.Target;
  * @author kfyty725
  * @date 2021/6/26 11:03
  * @email kfyty725@hotmail.com
+ * @see com.kfyty.loveqq.framework.boot.proxy.AsyncMethodInterceptorProxy
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Async {
     /**
-     * 执行该任务的线程池的 bean name，必须是 ExecutorService 的子类
+     * 执行该任务的线程池的 bean name，必须是 {@link java.util.concurrent.ExecutorService} 的子类
      */
     String value() default "";
+
+    /**
+     * 用于支持异步方法转同步
+     * 被注解的方法中，当在该方法体中(该方法的整个调用链路中)调用其他异步方法时，其他异步方法自动转为同步调用，而无需显示手动等待
+     * <p>
+     * 其他异步方法包含 {@link Async} 注解的方法，以及返回值为 {@link java.util.concurrent.Future}/{@link java.util.concurrent.CompletionStage} 的可代理方法
+     * <b>
+     * 仅在类上同时注释 {@link Async} 时，方法注释才有效
+     * </b>
+     * </p>
+     */
+    @Documented
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    @interface Await {
+        /**
+         * 由于整个调用链路有效，因此如果有某个异步方法必须异步，则可以再次注解该方法，并设置为 false，则可以覆盖之前的设置
+         *
+         * @return true/false
+         */
+        boolean value() default true;
+    }
 }
