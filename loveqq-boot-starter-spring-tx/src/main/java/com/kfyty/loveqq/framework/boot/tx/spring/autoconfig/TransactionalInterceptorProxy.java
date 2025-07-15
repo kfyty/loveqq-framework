948562e3efad25d2edd8c7c62d137122766e373b
kfyty725 <kfyty725@hotmail.com>
2025-07-15 19:36:25 +0800

    opt:tx proxy

--- loveqq-boot-starter-spring-tx/src/main/java/com/kfyty/loveqq/framework/boot/tx/spring/autoconfig/TransactionalInterceptorProxy.java
+++ loveqq-boot-starter-spring-tx/src/main/java/com/kfyty/loveqq/framework/boot/tx/spring/autoconfig/TransactionalInterceptorProxy.java
@@ -8,16 +8,23 @@ import com.kfyty.loveqq.framework.core.proxy.aop.AdviceMethodInterceptorChainPoi
 import com.kfyty.loveqq.framework.core.proxy.aop.MethodAroundAdvice;
 import com.kfyty.loveqq.framework.core.utils.AnnotationUtil;
 import com.kfyty.loveqq.framework.core.utils.CommonUtil;
+import com.kfyty.loveqq.framework.core.utils.ExceptionUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NoRollbackRuleAttribute;
+import org.springframework.transaction.interceptor.RollbackRuleAttribute;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
 import org.springframework.transaction.interceptor.TransactionAttribute;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 描述: 事务拦截代理
@@ -26,8 +33,12 @@ import java.util.Arrays;
  * @date 2021/7/29 13:07
  * @email kfyty725@hotmail.com
  */
+@Slf4j
 @RequiredArgsConstructor
 public class TransactionalInterceptorProxy implements AdviceMethodInterceptorChainPoint {
+    /**
+     * bean factory
+     */
     private final BeanFactory beanFactory;
 
     @Override
@@ -43,7 +54,7 @@ public class TransactionalInterceptorProxy implements AdviceMethodInterceptorCha
     @Override
     public Object proceed(MethodProxy methodProxy, MethodInterceptorChain chain) throws Throwable {
         Transactional transactional = this.obtainTransactional(methodProxy.getTargetMethod());
-        TransactionAttribute transactionAttribute = this.resolveTransactionAttribute(transactional);
+        TransactionAttribute transactionAttribute = this.obtainTransactionAttribute(transactional);
         if (transactionAttribute == null) {
             return chain.proceed(methodProxy);
         }
@@ -54,8 +65,9 @@ public class TransactionalInterceptorProxy implements AdviceMethodInterceptorCha
             platformTransactionManager.commit(transaction);
             return retValue;
         } catch (Throwable e) {
-            this.completeTransactionAfterThrowing(transaction, transactionAttribute, platformTransactionManager, e, transactional);
-            throw e;
+            Throwable unwrap = ExceptionUtil.unwrap(e);
+            this.completeTransactionAfterThrowing(transaction, transactionAttribute, platformTransactionManager, unwrap);
+            throw unwrap;
         }
     }
 
@@ -67,52 +79,73 @@ public class TransactionalInterceptorProxy implements AdviceMethodInterceptorCha
         return annotation;
     }
 
-    protected TransactionAttribute resolveTransactionAttribute(Transactional transactional) {
+    protected TransactionAttribute obtainTransactionAttribute(Transactional transactional) {
         if (transactional == null) {
             return null;
         }
-        DefaultTransactionAttribute transactionAttribute = new DefaultTransactionAttribute();
+
+        List<RollbackRuleAttribute> rollbackRuleAttributes = new LinkedList<>();
+        for (Class<? extends Throwable> clazz : transactional.rollbackFor()) {
+            rollbackRuleAttributes.add(new RollbackRuleAttribute(clazz));
+        }
+        for (String className : transactional.rollbackForClassName()) {
+            rollbackRuleAttributes.add(new RollbackRuleAttribute(className));
+        }
+        for (Class<? extends Throwable> clazz : transactional.noRollbackFor()) {
+            rollbackRuleAttributes.add(new NoRollbackRuleAttribute(clazz));
+        }
+        for (String className : transactional.noRollbackForClassName()) {
+            rollbackRuleAttributes.add(new NoRollbackRuleAttribute(className));
+        }
+
+        RuleBasedTransactionAttribute transactionAttribute = new RuleBasedTransactionAttribute();
         transactionAttribute.setIsolationLevel(transactional.isolation().value());
         transactionAttribute.setPropagationBehavior(transactional.propagation().value());
         transactionAttribute.setReadOnly(transactional.readOnly());
         transactionAttribute.setTimeout(transactional.timeout());
+        transactionAttribute.setLabels(Arrays.asList(transactional.label()));
+        transactionAttribute.setRollbackRules(rollbackRuleAttributes);
+
         return transactionAttribute;
     }
 
     protected PlatformTransactionManager obtainPlatformTransactionManager(Transactional transactional) {
         String beanName = transactional.value();
-        if (CommonUtil.empty(beanName)) {
+        if (beanName == null || beanName.isEmpty()) {
             beanName = transactional.transactionManager();
         }
         Object bean = CommonUtil.notEmpty(beanName) ? this.beanFactory.getBean(beanName) : this.beanFactory.getBean(PlatformTransactionManager.class);
-        if (bean == null) {
-            throw new IllegalArgumentException("The PlatformTransactionManager does not exists !");
+        if (!(bean instanceof PlatformTransactionManager)) {
+            throw new IllegalArgumentException("The PlatformTransactionManager doesn't exists.");
         }
         return (PlatformTransactionManager) bean;
     }
 
-    protected void completeTransactionAfterThrowing(TransactionStatus transaction, TransactionAttribute ta, PlatformTransactionManager tm, Throwable ex, Transactional transactional) {
-        Boolean rollback = this.rollbackFor(ex, transactional);
-        if (rollback != null && rollback || rollback == null && ta.rollbackOn(ex)) {
-            tm.rollback(transaction);
+    protected void completeTransactionAfterThrowing(TransactionStatus transaction, TransactionAttribute transactionAttribute, PlatformTransactionManager transactionManager, Throwable ex) {
+        if (transactionAttribute.rollbackOn(ex)) {
+            try {
+                transactionManager.rollback(transaction);
+            } catch (TransactionSystemException e) {
+                log.error("Application exception overridden by rollback exception", ex);
+                e.initApplicationException(ex);
+                throw e;
+            } catch (RuntimeException | Error throwable) {
+                log.error("Application exception overridden by rollback exception", ex);
+                throw throwable;
+            }
+            return;
         }
-    }
 
-    protected Boolean rollbackFor(Throwable ex, Transactional transactional) {
-        Class<?> exClass = ex.getClass();
-        String exName = exClass.getName();
-        if (CommonUtil.notEmpty(transactional.rollbackFor())) {
-            return Arrays.stream(transactional.rollbackFor()).anyMatch(e -> e.isAssignableFrom(exClass));
+        // don't roll back on this exception, and roll back if TransactionStatus.isRollbackOnly() is true.
+        try {
+            transactionManager.commit(transaction);
+        } catch (TransactionSystemException e) {
+            log.error("Application exception overridden by commit exception", ex);
+            e.initApplicationException(ex);
+            throw e;
+        } catch (RuntimeException | Error throwable) {
+            log.error("Application exception overridden by commit exception", ex);
+            throw throwable;
         }
-        if (CommonUtil.notEmpty(transactional.rollbackForClassName())) {
-            return Arrays.stream(transactional.rollbackForClassName()).anyMatch(e -> e.equals(exName));
-        }
-        if (CommonUtil.notEmpty(transactional.noRollbackFor())) {
-            return Arrays.stream(transactional.noRollbackFor()).noneMatch(e -> e.isAssignableFrom(exClass));
-        }
-        if (CommonUtil.notEmpty(transactional.noRollbackForClassName())) {
-            return Arrays.stream(transactional.noRollbackForClassName()).noneMatch(e -> e.equals(exName));
-        }
-        return null;
     }
 }
