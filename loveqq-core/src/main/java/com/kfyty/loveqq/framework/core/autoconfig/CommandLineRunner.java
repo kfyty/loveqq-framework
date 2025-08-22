ef2be41dacc5b2f41e6a6fb06bf8a55620d93f64
kfyty725 <kfyty725@hotmail.com>
2025-08-22 20:55:50 +0800

    opt:@FeignClient支持设置是否懒加载

--- loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/CommandLineRunner.java
+++ loveqq-core/src/main/java/com/kfyty/loveqq/framework/core/autoconfig/CommandLineRunner.java
@@ -1,12 +1,18 @@
 package com.kfyty.loveqq.framework.core.autoconfig;
 
 /**
- * 描述: CommandLineRunner
+ * 描述: 应用启动完成后执行的初始化命令
  *
  * @author kfyty725
  * @date 2021/7/4 18:49
  * @email kfyty725@hotmail.com
  */
 public interface CommandLineRunner {
+    /**
+     * 运行任务
+     *
+     * @param args 命令行参数
+     * @throws Exception 可能抛出的异常
+     */
     void run(String... args) throws Exception;
 }
