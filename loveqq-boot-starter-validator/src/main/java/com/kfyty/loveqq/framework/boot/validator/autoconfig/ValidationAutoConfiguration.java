c224324de38433271cb1587edbf8dc6bbc7647fc
kfyty725 <kfyty725@hotmail.com>
2025-10-03 15:11:06 +0800

    feat:jar校验器支持国际化配置

--- loveqq-boot-starter-validator/src/main/java/com/kfyty/loveqq/framework/boot/validator/autoconfig/ValidationAutoConfiguration.java
+++ loveqq-boot-starter-validator/src/main/java/com/kfyty/loveqq/framework/boot/validator/autoconfig/ValidationAutoConfiguration.java
@@ -4,20 +4,27 @@ import com.kfyty.loveqq.framework.boot.validator.context.IOCContext;
 import com.kfyty.loveqq.framework.boot.validator.context.ValidatorContext;
 import com.kfyty.loveqq.framework.boot.validator.proxy.ValidatorAccess;
 import com.kfyty.loveqq.framework.boot.validator.proxy.ValidatorProxy;
+import com.kfyty.loveqq.framework.core.autoconfig.annotation.Autowired;
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.Bean;
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.Configuration;
 import com.kfyty.loveqq.framework.core.autoconfig.annotation.Value;
 import com.kfyty.loveqq.framework.core.autoconfig.condition.annotation.ConditionalOnMissingBean;
 import com.kfyty.loveqq.framework.core.event.ApplicationListener;
 import com.kfyty.loveqq.framework.core.event.ContextRefreshedEvent;
+import com.kfyty.loveqq.framework.core.i18n.I18nResourceBundle;
 import com.kfyty.loveqq.framework.core.lang.ConstantConfig;
 import com.kfyty.loveqq.framework.core.utils.ClassLoaderUtil;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.BaseHibernateValidatorConfiguration;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.spi.resourceloading.ResourceBundleLocator;
 
 import java.lang.reflect.Proxy;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * 描述: 校验器配置
@@ -39,8 +46,15 @@ public class ValidationAutoConfiguration implements ApplicationListener<ContextR
 
     @ConditionalOnMissingBean
     @Bean(resolveNested = false, independent = true)
-    public ValidatorFactory validatorFactory(jakarta.validation.Configuration<?> configure) {
+    public ValidatorFactory validatorFactory(jakarta.validation.Configuration<?> configure, @Autowired(required = false) I18nResourceBundle i18nResourceBundle) {
+        // 配置国际化
+        if (i18nResourceBundle != null) {
+            configure.messageInterpolator(new ResourceBundleMessageInterpolator(new MessageSourceResourceBundleLocator(i18nResourceBundle)));
+        }
+
+        // 配置快速失败
         configure.addProperty(BaseHibernateValidatorConfiguration.FAIL_FAST, Boolean.TRUE.toString());
+
         return configure.buildValidatorFactory();
     }
 
@@ -65,5 +79,19 @@ public class ValidationAutoConfiguration implements ApplicationListener<ContextR
             return validator;
         }
         return (Validator) Proxy.newProxyInstance(ClassLoaderUtil.classLoader(this.getClass()), new Class[]{Validator.class}, new ValidatorProxy(new ValidatorAccess(validator)));
+    }
+
+    @RequiredArgsConstructor
+    protected static class MessageSourceResourceBundleLocator implements ResourceBundleLocator {
+        /**
+         * 国际化资源
+         */
+        protected final I18nResourceBundle i18nResourceBundle;
+
+        @Override
+        public ResourceBundle getResourceBundle(Locale locale) {
+            ResourceBundle[] resourceBundle = this.i18nResourceBundle.getResourceBundle(locale);
+            return resourceBundle.length < 1 ? null : resourceBundle[0];
+        }
     }
 }
